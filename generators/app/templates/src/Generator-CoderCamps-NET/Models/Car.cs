fbd6d13d0088a290a697d9c8de5f5e62e11c677d
Jeremy Doucet <jeremy.doucet93@gmail.com>
2016-03-02 15:49:39 -0800

    fixed namespaces for models

--- generators/app/templates/src/Generator-CoderCamps-NET/Models/Car.cs
+++ generators/app/templates/src/Generator-CoderCamps-NET/Models/Car.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Generator_CoderCamps_NET.Models
+namespace <%= appNamespace %>.Models
 {
     public class Car
     {
